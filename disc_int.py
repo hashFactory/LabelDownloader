d859c7f6ebe9d98b64cc679ca468b8673c599463
Tristan Charpentier <tristan_charpentier@hotmail.com>
2020-09-14 05:08:43 +0200

    checking return values for dupes

--- disc_int.py
+++ disc_int.py
@@ -12,6 +12,7 @@ from collections import namedtuple
 #testing stuff
 import difflib
 import hashlib
+import textdistance
 
 # load jsut for testing
 file = open('oauth_works.pickle', 'rb')
@@ -22,7 +23,7 @@ label = d.label(14215)
 
 # print releases to check all is well
 releases = label.releases
-
+rev = releases.reverse()
 
 #make sure that releases are unique
 #hash_release 
@@ -54,8 +55,19 @@ def print_release(rel):
         print(("\t\t  " + str(index) + ". " + t.title))
         index += 1
 
+#returns hashable version of release
+def release_to_hashable(rel):
+    pass
+
 for r in releases:
-    if str(r.title).upper() not in r_master_names:
+    #if str(r.title).upper() not in r_master_names:
+    if not r_master_names:
+        #check for "collision"
+        #do it better with lambda functions but too lazy to rn
+        minimum_diff = 1000
+        diff = 1000
+        for test in r_master_names:
+            diff = textdistance.damerau_levenshtein(r.title)
         r_master_names.append(str(r.title).upper())
         r_clean.append(r)
         print("|" + r.title + "|")
